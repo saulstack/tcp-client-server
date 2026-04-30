e9acd8eff01fa9beec7d9a05dd7ff53d3dcf3f06
saulstack <279903240+saulstack@users.noreply.github.com>
2026-04-29 22:31:50 -0600

    v3: add file metadata and progress tracking

--- client.py
+++ client.py
@@ -1,20 +1,52 @@
 import socket
+from pathlib import Path
+
+
+def recv_exact(sock, num_bytes):
+    data = b''
+
+    while len(data) < num_bytes:
+        chunk = sock.recv(num_bytes - len(data))
+
+        if not chunk:
+            raise ConnectionError('Connection closed before receiving expected data.')
+
+        data += chunk
+
+    return data
 
 SERVER_HOST = '127.0.0.1'
 SERVER_PORT = 20000
 BUFFER_SIZE = 4096
-OUTPUT_FILE = 'received_file.pdf'
+ENCODING = 'utf-8'
+PATH_TO_RECEIVED_FILE = Path('.')
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
+with (socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket):
     client_socket.connect((SERVER_HOST, SERVER_PORT))
 
-    with open(OUTPUT_FILE, 'wb') as file:
-        while True:
-            chunk = client_socket.recv(BUFFER_SIZE)
+    raw_len_name = recv_exact(client_socket,4)
+    file_name_len = int.from_bytes(raw_len_name, 'big')
+    name_bytes = recv_exact(client_socket, file_name_len)
+
+
+    FILE_NAME = name_bytes.decode(ENCODING)
+    RECEIVED_FILE = PATH_TO_RECEIVED_FILE / Path(FILE_NAME).name
+
+    DATA_SIZE = recv_exact(client_socket, 8)
+    FILE_SIZE = int.from_bytes(DATA_SIZE, byteorder='big')
+
+    current_received_bytes = 0
+    with open(RECEIVED_FILE, 'wb') as file:
+        while current_received_bytes < FILE_SIZE:
+            remaining_bytes = FILE_SIZE - current_received_bytes
+            chunk = client_socket.recv(min(BUFFER_SIZE, remaining_bytes))
 
             if not chunk:
-                break
+                raise ConnectionError('Connection closed before full file was received.')
 
             file.write(chunk)
+            current_received_bytes += len(chunk)
+            percentage = (current_received_bytes / FILE_SIZE) * 100
+            print(f'\rProgress: {percentage:.2f}%',  end = '')
 
-print('File received successfully.')
+    print('\nFile received successfully.')
--- server.py
+++ server.py
@@ -1,9 +1,11 @@
 import socket
+from pathlib import Path
 
 SERVER_HOST = ''
 SERVER_PORT = 20000
 BUFFER_SIZE = 4096
-FILE_PATH = 'FILE.PDF'
+FILE_PATH = Path('file.mp4')
+ENCODING = 'utf-8'
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
     server_socket.bind((SERVER_HOST, SERVER_PORT))
@@ -15,6 +17,15 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
 
     with connection_socket:
         print(f'Connection established with {address}')
+
+        name_bytes = FILE_PATH.name.encode(ENCODING)
+        length_name = len(name_bytes)
+        connection_socket.sendall(length_name.to_bytes(4, 'big'))
+        connection_socket.sendall(name_bytes)
+
+        FILE_SIZE = FILE_PATH.stat().st_size
+        connection_socket.sendall(FILE_SIZE.to_bytes(8, 'big'))
+
 
         with open(FILE_PATH, 'rb') as file:
             while True:
